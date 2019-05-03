93d912353faef71aff0a816153a8bee05981fba5
Jean Sarlôn <jean.sarlon@icloud.com>
2019-05-03 09:22:49 -0300

    Removendo deprecated WebMvcConfigurerAdapter

--- src/main/java/com/thoughtworks/aceleradora/config/WebConfig.java
+++ src/main/java/com/thoughtworks/aceleradora/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.resource.ContentVersionStrategy;
 import org.springframework.web.servlet.resource.VersionResourceResolver;
@@ -12,12 +13,12 @@ import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         final VersionResourceResolver versionResolver = new VersionResourceResolver();
-//        versionResolver.addFixedVersionStrategy("test_version", "/**");
+        versionResolver.addFixedVersionStrategy("test_version", "/**");
 
         VersionResourceResolver versionResourceResolver = new VersionResourceResolver()
                 .addVersionStrategy(new ContentVersionStrategy(),"/**");
@@ -27,32 +28,6 @@ public class WebConfig extends WebMvcConfigurerAdapter {
                 .setCachePeriod(60*60*24*365)
                 .resourceChain(true)
                 .addResolver(versionResourceResolver);
-
-//        registry.addResourceHandler(
-//                "/**",
-//                "/assets/**"
-//        )
-//        .addResourceLocations(
-//            "classpath:/static/assets/",
-//                "classpath:/static/"
-//        );
-
-
-//        registry.addResourceHandler(
-//                "/**",
-//                "/assets/**",
-//                "/assets/**/*.js",
-//                "/assets/**/*.css")
-//        .addResourceLocations(
-//                "classpath:/static/assets/",
-//                "classpath:/static/assets/js/",
-//                "classpath:/static/assets/css")
-//        .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS))
-//        .resourceChain(false)
-//        .addResolver(versionResolver);
-
-
-
     }
 
 }
